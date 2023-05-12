b505ee6daa85b4061ba30265ee1b18d2c1ab3ce4
richardpan <richard.pan@dominos.com.au>
2023-05-12 23:40:30 +1000

    Define themes

---
+++ src/themes/themes.ts
@@ -0,0 +1,24 @@
+export const colorScheme = 'teal';
+
+export const colors = {
+	white: '#FFFFFF',
+	backgroundGray: '#F7FAFC',
+	borderGray: '#E2E8F0',
+	primaryGray: '#1A202C',
+	secondaryGray: '#4A5568',
+	primaryGreen: '#2C7A7B',
+};
+
+export const sizes = {
+	px: '1px',
+	halfQuarter: '0.125rem',
+	quarter: '0.25rem',
+	half: '0.5rem',
+	one: '1rem',
+	oneQuarter: '1.25rem',
+	oneHalf: '1.5rem',
+	double: '2rem',
+	triple: '3rem',
+};
+
+export const fontFamily = 'Roboto Mono';
