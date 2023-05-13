3b71fa10df5963d3ab91d9d1d983b5dd8b082b8d
richardpan <richard.pan@dominos.com.au>
2023-05-13 10:37:08 +1000

    Add homepage tests to render UI correctly

--- src/pages/Home/Home.spec.tsx
+++ src/pages/Home/Home.spec.tsx
@@ -1,1 +1,21 @@
-export {}
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home from './Home';
+
+test('should render heading', () => {
+	render(<Home />);
+	const heading = screen.getByText(/A better way/i);
+	expect(heading).toBeInTheDocument();
+});
+
+test('should render sub-heading', () => {
+	render(<Home />);
+	const subheading = screen.getByText(/Be the first to know when we launch./i);
+	expect(subheading).toBeInTheDocument();
+});
+
+test('should render request invite button', () => {
+	render(<Home />);
+	const requestButton = screen.getByRole('button', { name: /Request an invite/i });
+	expect(requestButton).toBeInTheDocument();
+});
