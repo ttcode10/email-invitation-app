53ba6b654b57ce947ff64bb36de00b73f2d8dd1a
richardpan <richard.pan@dominos.com.au>
2023-05-14 21:35:35 +1000

    Reinforce email validation rules

--- src/pages/Home/schemas/form-validation.ts
+++ src/pages/Home/schemas/form-validation.ts
@@ -1,11 +1,17 @@
 import * as yup from 'yup';
 
+const emailRules =
+	/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 export const formSchema = yup.object().shape({
 	name: yup
 		.string()
 		.min(3, 'Name must be at least 3 characters long')
 		.required('Required'),
-	email: yup.string().email('Please enter a valid email').required('Required'),
+	email: yup
+		.string()
+		.matches(emailRules, { message: 'Please enter a valid email' })
+		.required('Required'),
 	confirmEmail: yup
 		.string()
 		.oneOf([yup.ref('email'), undefined], 'Emails must match')
